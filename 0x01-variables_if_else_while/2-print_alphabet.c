a880703f14643578bd3f3c480feb197573287bc7
Nihalmadida <aliyuwalida0@gmail.com>
2023-06-18 01:38:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main- prints alphabets in lower case
+*
+*Return: always (success)
+*/
+int main(void)
+{
+char alph[26]
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	(int i = 0 i < 26, i++)
+	putchar(lowercase alphabet "a,b,c..z", \n, "a,b,c..z")
+	putchar('\n');
+	return (0);
+}
